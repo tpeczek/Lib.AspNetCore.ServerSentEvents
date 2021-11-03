c90956970de93fa51553eb8dce84ac7cfb241846
Tomasz Pęczek <tpeczek@gmail.com>
2021-11-03 22:46:02 +0100

    Adding test for race condition when managing groups (#43)

---
+++ Test.AspNetCore.ServerSentEvents/ServerSentEventsServiceTests.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
+using Xunit;
+using Lib.AspNetCore.ServerSentEvents;
+using Lib.AspNetCore.ServerSentEvents.Internals;
+
+namespace Test.AspNetCore.ServerSentEvents
+{
+    public class ServerSentEventsServiceTests
+    {
+        #region Prepare SUT
+        private static async Task<ServerSentEventsClient> PrepareAndAddServerSentEventsClientAsync(ServerSentEventsService serverSentEventsService)
+        {
+            HttpContext context = new DefaultHttpContext();
+            ServerSentEventsClient serverSentEventsClient = new ServerSentEventsClient(Guid.NewGuid(), new ClaimsPrincipal(), context.Response, false);
+            
+            await serverSentEventsService.OnConnectAsync(context.Request, serverSentEventsClient);
+
+            serverSentEventsService.AddClient(serverSentEventsClient);
+
+            return serverSentEventsClient;
+        }
+        #endregion
+
+        #region Tests
+        [Fact]
+        public void RemoveClient_ClientsBeingAddedToGroupsInParaller_NoRaceCondition()
+        {
+            // ARRANGE
+            ServerSentEventsService serverSentEventsService = new ServerSentEventsService(Options.Create<ServerSentEventsServiceOptions<ServerSentEventsService>>(new ServerSentEventsServiceOptions<ServerSentEventsService>
+            {
+                OnClientConnected = async (service, clientConnectedArgs) =>
+                {
+                    for (var i = 0; i < 1000; i++)
+                    {
+                        await service.AddToGroupAsync(Guid.NewGuid().ToString(), clientConnectedArgs.Client);
+                    }
+                }
+            }));
+
+            // ACT
+            Task[] clientsTasks = new Task[100];
+            for (int i = 0; i < clientsTasks.Length; i++)
+            {
+                clientsTasks[i] = Task.Run(async () =>
+                {
+                    ServerSentEventsClient serverSentEventsClient = await PrepareAndAddServerSentEventsClientAsync(serverSentEventsService);
+
+                    await Task.Delay(100);
+
+                    serverSentEventsService.RemoveClient(serverSentEventsClient);
+                });
+            }
+            Exception recordedException = Record.Exception(() => Task.WaitAll(clientsTasks));
+
+            // ASSERT
+            Assert.False((recordedException as AggregateException)?.InnerExceptions.Any(ex => (ex as InvalidOperationException)?.Message.Contains("Collection was modified") ?? false) ?? false);
+        }
+
+        [Fact]
+        public async Task GetClients_GroupNameProvidedAndClientInGroup_ReturnsClient()
+        {
+            // ARRANGE
+            const string serverSentEventsClientsGroupName = nameof(GetClients_GroupNameProvidedAndClientInGroup_ReturnsClient);
+            ServerSentEventsService serverSentEventsService = new ServerSentEventsService(Options.Create<ServerSentEventsServiceOptions<ServerSentEventsService>>(new ServerSentEventsServiceOptions<ServerSentEventsService>
+            {
+                OnClientConnected = async (service, clientConnectedArgs) =>
+                {
+                    await service.AddToGroupAsync(serverSentEventsClientsGroupName, clientConnectedArgs.Client);
+                }
+            }));
+            ServerSentEventsClient serverSentEventsClient = await PrepareAndAddServerSentEventsClientAsync(serverSentEventsService);
+
+            // ACT
+            IReadOnlyCollection<IServerSentEventsClient> serverSentEventsClientsGroup = serverSentEventsService.GetClients(serverSentEventsClientsGroupName);
+
+            // ASSERT
+            Assert.Single(serverSentEventsClientsGroup, serverSentEventsClient);
+        }
+        #endregion
+    }
+}
